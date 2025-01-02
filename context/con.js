37dbda9fb2e7dd3aa28414b5a9bb7390ad9a84a2
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2025-01-02 19:43:11 +0530

    Add files via upload

---
+++ context/con.js
@@ -0,0 +1,7 @@
+///0x5FbDB2315678afecb367f032d93F642f64180aa3///
+import voterabi from "../artifacts/contracts/VotingDApp.sol/VotingDApp.json";
+
+export const votingAddress ="0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+
+export const votingABI =voterabi.abi;
